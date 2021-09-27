da9227fd8580b316cf8bd82662ba467bba0ddbdc
Hafizh Aradhana Harimurti <mochikacang1@mail.ugm.ac.id>
2021-09-27 11:43:11 +0700

    Added GET queue, connection logging for DB, and capability to run on localhost:3000

--- server.js
+++ server.js
@@ -6,4 +6,17 @@ const url = "mongodb+srv://dbUserTugas:Pass_db_User@paw-kelompok-2.wmcaw.mongodb
 const Queue = require('./models/Queue');
 const Patients = require('./models/Patients');
 
-mongoose.connect(url,{useNewUrlParser:true});
+mongoose.connect(url,{useNewUrlParser:true})
+    .then(res => console.log('Connected to DB'))
+    .catch(err => console.log(err));
+
+//Create localhost:3000
+app.listen(3000);
+
+//GET queue
+app.get('/queue',async(req,res)=>{
+    const queueItems = await Queue.find();
+    res.send(queueItems);
+});
+
+//PUT queue
